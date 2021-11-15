11742c912f2c3a9ad4dd6240874a760bad6604d5
Anna Larus <77969817+anlarus@users.noreply.github.com>
2021-11-16 03:36:34 +0400

    feat: add dinamic data managing started

--- devs-united/src/App.js
+++ devs-united/src/App.js
@@ -2,23 +2,42 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { firestore } from "./firebase";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [tweets, setTweets] = useState([]);
   const [body, setBody] = useState({});
+  const [alarm, setAlarm] = useState("success");
+  const user = useRef();
 
-  const getAllTweets = () => {};
+  const getAllTweets = () => {
+    firestore
+      .collection("tweets")
+      // .limit(1)
+      // .where('likes', '>', number)
+      // .orderBy('likes', 'asc')
+      // .startAt(30)
+      // .endAt(50)
+      .get()
+      .then((snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          return {
+            message: doc.data().message,
+            user: doc.data().user,
+            id: doc.id,
+          };
+        });
+        setTweets(tweets);
+      });
+  };
 
   const createTweet = (e) => {
     e.preventDefault();
-    let tweet = firestore
+    firestore
       .collection("tweets")
       .add(body)
-      .then((docRef) => {
-        return docRef.get();
-      })
-      .catch(error => console.error("has occured an", error))
+      .then(getAllTweets())
+      .catch((error) => console.error("has occured an", error));
   };
 
   const handleOnChange = () => {
@@ -28,6 +47,15 @@ function App() {
     };
     setBody(newTweet);
   };
+
+  const deleteTweet=()=> {
+firestore.collection("tweets")
+.doc(id)
+.get()
+.delete()
+.then(getAllTweets())
+.catch((error) => console.error("has occured an", error));
+  }
 
   let number = 20;
 
@@ -53,9 +81,23 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    getAllTweets();
+  }, []);
+
   return (
     <Router>
       <div className="App">
+        {tweets.map((tweet) => {
+          return (
+            <div>
+              <div className="who">{tweet.user}</div>
+              <div className="what">{tweet.message}</div>
+              <div className="id">{tweet.id}</div>
+              <button onClick={()=>deleteTweet(tweet.id)}>erase</button>
+            </div>
+          )
+        })}
         <form onSubmit={createTweet}>
           <input name="user" onChange={handleOnChange} type="text"></input>
           <textarea name="message" onChange={handleOnChange}></textarea>
