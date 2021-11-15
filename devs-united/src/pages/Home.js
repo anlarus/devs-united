a36a6ad8c1de23cb21c83e90a8d3feaa98ef90a6
Anna Larus <77969817+anlarus@users.noreply.github.com>
2021-11-15 08:59:07 +0400

    feat: start make homepage and  add link to logo

---
+++ devs-united/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = ()=> {
+    return (
+        <main>
+    <img src="../src/assets/images/bigLogo.svg" alt="the devs united simbol"/>
+        </main>
+    )
+}
+
+export default Home
