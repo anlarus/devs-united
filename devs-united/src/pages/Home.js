4ad2effe778b1b7a11decbece3f4b570a4144b2a
Anna Larus <77969817+anlarus@users.noreply.github.com>
2021-11-17 10:52:23 +0400

    feat: add font face and font style

--- devs-united/src/pages/Home.js
+++ devs-united/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Link, Router } from "react-router-dom";
+import "./Home.css"
+import { Link } from "react-router-dom";
 import { GoogleLogin } from "react-google-login";
+import {ReactComponent as BigLogo} from "../assets/images/bigLogo.svg";
 
 const Home = () => {
   const responseGoogle = (response) => {
@@ -9,16 +11,14 @@ const Home = () => {
 
   return (
    
-      <main>
+      <main className="font-face-silk">
         <div></div>
+
+        <Link to="/">
+          <BigLogo/>
+        </Link>
         <h1>lorem ipsum dolor</h1>
         <p>lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-        <Link to="/">
-          <img
-            src="../assets/images/bigLogo.svg"
-            alt="the devs united simbol"
-          />
-        </Link>
         <GoogleLogin
           clientId="141550570435-721ct27r4e9u6ifl7heom8cm8dr4r4h9.apps.googleusercontent.com"
           buttonText="Login"
