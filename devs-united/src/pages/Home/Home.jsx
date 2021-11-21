0679d1b2dcaac80fed6c2479b24acc2ede888ec4
Anna Larus <77969817+anlarus@users.noreply.github.com>
2021-11-21 22:19:59 +0400

    feat: save the changed files

--- devs-united/src/pages/Home/Home.jsx
+++ devs-united/src/pages/Home/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         <GoogleLogin
           clientId="141550570435-721ct27r4e9u6ifl7heom8cm8dr4r4h9.apps.googleusercontent.com"
           render={renderProps => (
-            <div className="button-cover"><img class="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/><button className="google-button" onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Google</button></div>
+            <div className="button-cover"><div className="google-logo-cover"><img class="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/></div><button className="google-button" onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign in with Google</button></div>
           )}
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
